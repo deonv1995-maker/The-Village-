b51dbb692748e9ecc2402be00e0152bedc7ce4c2
deonv1995-maker <deonv1995@gmail.com>
2026-09-04 10:30:47 +0200

    Add shared world and movement gameplay config

---
+++ src/data/gameplayConfig.js
@@ -0,0 +1,10 @@
+export const GAMEPLAY_CONFIG = Object.freeze({
+  world: Object.freeze({
+    size: 90,
+    groundY: 0
+  }),
+  movement: Object.freeze({
+    villagerSpeed: 3.2,
+    arrivalThreshold: 0.06
+  })
+});
